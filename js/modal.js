a7dc2d947b9be4c2aef44999f7388604c5cb94df
Bardya Vahydy <vahydy.bardya97@gmail.com>
2023-11-23 18:26:00 +0330

    add modal js codes

---
+++ js/modal.js
@@ -0,0 +1,38 @@
+const body = document.body
+
+const createModal = (message ,classes , bgColor) =>{
+    let containerModal = document.createElement('div')
+    containerModal.className = 'center container-modal'
+    containerModal.style.animation = 'modalAnimation 10s linear'
+
+    let tickElmModal = document.createElement('i')
+    tickElmModal.className = `${classes} mr-svg`
+    tickElmModal.style.background = bgColor
+    tickElmModal.style.animation = 'modalIconAnimation .5s linear'
+    if(window.innerWidth < 370) tickElmModal.style.display = 'none'
+
+    let messageModal = document.createElement('p')
+    messageModal.className = 'container-modal__message-modal'
+    messageModal.innerText = message
+
+    let lineTimeModal = document.createElement('div')
+    lineTimeModal.classList.add('container-modal__line')
+    lineTimeModal.style.background = bgColor
+    lineTimeModal.style.animation = 'modalLineAnimation 10s linear'
+
+    containerModal.append(tickElmModal, messageModal, lineTimeModal)
+    body.append(containerModal)
+
+    setTimeout(() => {
+        containerModal.remove()
+    }, 10000);
+}
+
+const removeModal = () =>{
+    if(document.querySelector('.container-modal')){
+        const containerModal = document.querySelector('.container-modal')
+        containerModal.style.animation = ''
+    }
+}
+
+export { createModal, removeModal }
