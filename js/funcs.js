d82bbc39d2130a66753c2cf04bee1a8472fb2624
Bardya Vahydy <vahydy.bardya97@gmail.com>
2023-11-23 18:24:40 +0330

    add funcs js codes

---
+++ js/funcs.js
@@ -0,0 +1,16 @@
+const $ = document
+const addClass = (elm, className) => elm.classList.add(className) 
+const removeClass = (elm, className) => elm.classList.remove(className) 
+
+function createCircleForBtn(event, elm, widthElm){
+    let circle = $.createElement('div')
+    let readMoreBtnPosition = elm.getBoundingClientRect() 
+    elm.style.overflow = 'hidden'
+    if(elm.style.position !== 'relative')elm.style.position = 'relative'
+    circle.classList.add('circle')
+    circle.style.cssText = `width: ${widthElm * 0.1}rem; height: ${widthElm * 0.1}rem; left: ${(event.clientX - readMoreBtnPosition.left - (widthElm / 2))}px; top: ${(event.clientY - readMoreBtnPosition.top - (widthElm / 2))}px`
+    elm.appendChild(circle)
+    circle.onanimationend = () => circle.remove() 
+}
+
+export { createCircleForBtn, addClass, removeClass } 
