cfa8ca421762c9eed7f203140e90a028e34fa5c5
Bardya Vahydy <vahydy.bardya97@gmail.com>
2023-11-23 18:25:32 +0330

    add index js codes

---
+++ js/index.js
@@ -0,0 +1,12 @@
+'use strict'
+
+import { HeaderSite } from "../components/Header/header-site.js";
+
+window.customElements.define('header-site', HeaderSite)
+
+// functions 
+
+
+
+
+// event handler 
