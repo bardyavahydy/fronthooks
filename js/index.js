595e1fc8d947d2495656b616c930b89500809b86
Bardya Vahydy <vahydy.bardya97@gmail.com>
2023-11-27 13:21:03 +0330

    add swiper.js lib to project & define course-site tag to browser

--- js/index.js
+++ js/index.js
@@ -1,12 +1,31 @@
 'use strict'
 
 import { HeaderSite } from "../components/Header/header-site.js";
+import { CourseSite } from "../components/Course/course-site.js";
+
+const $ = document
 
 window.customElements.define('header-site', HeaderSite)
+window.customElements.define('course-site', CourseSite)
 
 // functions 
 
-
+const swiperCourse = new Swiper('.swiper-course', {
+    direction: 'horizontal',
+    slidesPerView: 'auto',
+    autoplay: {
+      delay: 3000,
+    },     
+    breakpoints: {
+      810: {
+        spaceBetween: 40,
+      },
+      
+      150: {
+        spaceBetween: 20,
+      }
+    }
+  });
 
 
 // event handler 
