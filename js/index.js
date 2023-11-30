9a318352003ff0a8d299106505e7519b32c045b7
Bardya Vahydy <vahydy.bardya97@gmail.com>
2023-11-30 11:11:08 +0330

    Import createCircleForBtn & set click event to bannerLetsGoLink and allCoursesLink

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
 'use strict'
 
+import { createCircleForBtn } from "../../js/funcs.js";
 import { HeaderSite } from "../components/Header/header-site.js";
 import { CourseSite } from "../components/Course/course-site.js";
 
 const $ = document
+const bannerLetsGoLink = $.querySelector('.container-banner-lets-go__link')
+const allCoursesLink = $.querySelector('.courses-wrapper-right__all-courses-link')
 
 window.customElements.define('header-site', HeaderSite)
 window.customElements.define('course-site', CourseSite)
@@ -29,3 +32,11 @@ const swiperCourse = new Swiper('.swiper-course', {
 
 
 // event handler 
+
+bannerLetsGoLink.addEventListener('click', function(event){
+  createCircleForBtn(event, this, this.offsetWidth)
+})
+
+allCoursesLink.addEventListener('click', function(event){
+  createCircleForBtn(event, this, this.offsetWidth)
+})
