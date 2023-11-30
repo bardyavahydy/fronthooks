384b5093e19845a2bc897980b3e9137dac052e0a
Bardya Vahydy <vahydy.bardya97@gmail.com>
2023-11-30 11:07:48 +0330

    Create new Api & add .json at the end of link & rename: allUsers => data, allUserInWeb => table, getAllusers => getAllData, postUser => postData & create errorHandler, putData, deleteData funcs & remove removeModal

--- js/HTTPreq.js
+++ js/HTTPreq.js
@@ -1,32 +1,57 @@
 import { createModal } from "./modal.js" 
 
-const BASE_URL = 'https://fronthooks2-default-rtdb.firebaseio.com/'
+const BASE_URL = 'https://fronthooks3-default-rtdb.firebaseio.com/'
 
-const getAllUsers = async (allUsersInWeb) =>{
+const getAllData = async (table) =>{
     try{
-        let res = await fetch(`${BASE_URL}${allUsersInWeb}`)
-        let allUsers = await res.json()
-        return allUsers
+        let res = await fetch(`${BASE_URL}${table}.json`)
+        let data = await res.json()
+        return data
     }catch(err){
-        createModal('ارتباط به درستی برقرار نشد اطفا دوباره تلاش کنید . 🙏🏼', 'fa fa-close', '#ef4444')
+        errorHandler()
         setTimeout(() => location.reload(), 2000)
     }
 }
 
-const postUser = async (userData) =>{
+const postData = async (data, table) =>{
     try{
-        let res = await fetch(`https://fronthooks2-default-rtdb.firebaseio.com/allUsers.json`,{
+        let res = await fetch(`${BASE_URL}${table}.json`,{
             method:'POST',
             headers:{
                 "Content-type": 'application/json'
             },
-            body:JSON.stringify(userData)
+            body:JSON.stringify(data)
         })
-        console.log(res);
     }catch(err){
-        if(document.querySelector('.container-modal'))
-        createModal('ارتباط به درستی برقرار نشد اطفا دوباره تلاش کنید . 🙏🏼', 'fa fa-close', '#ef4444')
+        errorHandler()
     }
 }
 
-export { getAllUsers, postUser }
+const putData = async (data, table, courseId) =>{
+    try{
+        let res = await fetch(`https://fronthooks3-default-rtdb.firebaseio.com/${table}/${courseId}.json`,{
+            method:'PUT',
+            headers:{
+                "Content-type": 'application/json'
+            },
+            body:JSON.stringify(data)
+        })
+    }catch(err){
+        errorHandler()
+    }
+}
+
+const deleteData = async (table, CourseId) =>{
+    try{
+        let res = await fetch(`https://fronthooks3-default-rtdb.firebaseio.com/${table}/${CourseId}.json`,{
+            method:'DELETE'
+        })
+    }catch(err){
+        errorHandler()
+    }
+}
+
+const errorHandler = () => createModal('ارتباط به درستی برقرار نشد اطفا دوباره تلاش کنید . 🙏🏼', 'fa fa-close', '#ef4444')
+
+
+export { getAllData, postData, putData, deleteData }
