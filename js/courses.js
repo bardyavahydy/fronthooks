b9484529e8e19e628dd21caecbd94678db69d471
Bardya Vahydy <vahydy.bardya97@gmail.com>
2023-12-04 18:45:55 +0330

    Creating courses.js & import HeaderSite and FooterSite components

---
+++ js/courses.js
@@ -0,0 +1,10 @@
+'use strict'
+
+import { HeaderSite } from "../components/Header/header-site.js";
+import { FooterSite } from "../components/Footer/footer-site.js";
+
+
+const $ = document
+
+window.customElements.define('header-site', HeaderSite)
+window.customElements.define('footer-site', FooterSite)
