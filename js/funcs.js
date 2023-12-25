5708e19ddfc6056e63accac7e923e2cc86cfc540
Bardya Vahydy <vahydy.bardya97@gmail.com>
2023-12-25 21:15:43 +0330

    Definition of SetTheTime and SetTheDate functions

--- js/funcs.js
+++ js/funcs.js
@@ -17,4 +17,34 @@ const convertGregorianDateToSolar = (date) =>{
     return moment(date, 'YYYY-M-D').locale('fa').format('YYYY/M/D')
 }
 
-export { createCircleForBtn, addClass, removeClass, convertGregorianDateToSolar } 
+const SetTheDate = () =>{
+    let date = new Date()
+    let year = date.getFullYear()
+    let month = date.getMonth() + 1
+    let day = date.getDate()
+
+    let solarDate = convertGregorianDateToSolar(`${year}/${month}/${day}`)
+    year = +solarDate.slice(0, 4)
+    month = +solarDate.slice(solarDate.indexOf('/') + 1, solarDate.lastIndexOf('/'))
+    day = +solarDate.slice(solarDate.lastIndexOf('/') + 1)
+
+    if(month <= 9) month = `0${month}`
+    if(day <= 9) day = `0${day}`
+
+    return `${year}/${month}/${day}`
+}
+
+const SetTheTime = () =>{
+    let time = new Date()
+    let hours = time.getHours()
+    let minutes = time.getMinutes()
+    let seconds = time.getSeconds()
+
+    if(hours <= 9) hours = `0${hours}`
+    if(minutes <= 9) minutes = `0${minutes}`
+    if(seconds <= 9) seconds = `0${seconds}`
+
+    return `${hours}:${minutes}:${seconds}`
+}
+
+export { createCircleForBtn, addClass, removeClass, convertGregorianDateToSolar, SetTheDate, SetTheTime } 
